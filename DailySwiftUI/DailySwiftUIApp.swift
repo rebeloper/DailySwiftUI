2c44862a98c83885c07d658afb744a04d525bbaf
Alex Nagy <therebeloper@gmail.com>
2021-09-19 19:30:21 +0300

    Initial Commit

---
+++ DailySwiftUI/DailySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailySwiftUIApp.swift
+//  DailySwiftUI
+//
+//  Created by Alex Nagy on 19.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct DailySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
